028769aa447763880c47cc1d5827eb06fadc7471
DolotovaK <104764377+DolotovaK@users.noreply.github.com>
2022-10-09 10:44:04 +0000

    Deploying to gh-pages from @ DolotovaK/goit-js-hw-09@930cd44663e847fc6581559f28b86753273c1783 🚀

--- 03-promises.2014f328.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button");const e=document.querySelector(".form");console.log(e.elements);
-//# sourceMappingURL=03-promises.2014f328.js.map
--- 03-promises.62328381.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("button");var e=document.querySelector(".form");console.log(e.elements)}();
-//# sourceMappingURL=03-promises.62328381.js.map
